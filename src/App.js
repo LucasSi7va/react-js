7cb28b4443714325a3ec3da41a1cb91ec31f5a9a
LucasSi7va <lucassilvadesouza9999@gmail.com>
2024-09-03 17:16:46 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+  <main>
+  
+    exercicio3
+  <div className='title'>
+      <h1>Hello World!</h1>
+      </div>
+ 
+ <br></br>
+    exercicio 4
+  <div className='description'>
+    <p>description</p>
+  </div>
+
+ <br></br> 
+  exercicio 5
+  <section className='grid'></section>
+  
+  <br></br>
+  exercicio 6
+    <a href='#'><h2>Lucas</h2></a>
+
+
+  </main>
+
+);
+}
+
+export default App;
